86b5c0ef21c6b3a9cd778b0adb091022f8f3b191
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-01-06 09:14:26 -0500

    Finished user GET routes, moving to post

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -36,6 +36,32 @@ export const addRemoveFriend = async (req, res) => {
     const { id, friendId } = req.params;
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
+    /*
+    We add user if they are not already included in friends list
+    Otherwise we remove them if they already are there
+    */
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    /*
+    Reformatting friends lists
+    */
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 
@@ -44,6 +45,7 @@ app.post("/auth/register", upload.single("picture"), register);
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.user("/posts", postRoutes);
 
 /* MONGOOSE */
 const PORT = process.env.PORT || 6001;
